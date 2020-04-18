557c823bfbbf227764975972b482f6d377fec3c1
shittyboard <59323522+shittyboard@users.noreply.github.com>
2020-04-18 15:08:16 -0400

    Create tunactunactun.js

---
+++ js/tunactunactun.js
@@ -0,0 +1,30 @@
+$('p').linkify();
+$('#sidebar').linkify({
+    target: "_blank"
+});
+
+function insert(text)
+{
+    //Stolen from Wakaba
+	var textarea=document.forms[0].postText;
+	if(textarea)
+	{
+		if(textarea.createTextRange && textarea.caretPos) // IE
+		{
+			var caretPos=textarea.caretPos;
+			caretPos.text=caretPos.text.charAt(caretPos.text.length-1)==" "?text+" ":text;
+		}
+		else if(textarea.setSelectionRange) // Firefox
+		{
+			var start=textarea.selectionStart;
+			var end=textarea.selectionEnd;
+			textarea.value=textarea.value.substr(0,start)+text+textarea.value.substr(end);
+			textarea.setSelectionRange(start+text.length,start+text.length);
+		}
+		else
+		{
+			textarea.value+=text+" ";
+		}
+		textarea.focus();
+	}
+}
